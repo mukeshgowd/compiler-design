ecb7a44f2ec58931f6bad246d99b8fde45230f19
mukesh <113425349+mukeshgowd@users.noreply.github.com>
2022-09-23 11:18:54 +0530

    Update 8-shift reduce parsing .c

--- 8-shift reduce parsing .c
+++ 8-shift reduce parsing .c
@@ -91,19 +91,13 @@ int main()
 		
 		printf("\n$%s\t%s$\t", stk, a);
 		
-		// Call check function ..which will
-		// check the stack whether its contain
-		// any production or not
 		check();
 	}
 	
-	// Rechecking last time if contain
-	// any valid production then it will
-	// replace otherwise invalid
+	
 	check();
 	
-	// if top of the stack is E(starting symbol)
-	// then it will accept the input
+
 	if(stk[0] == 'E' && stk[1] == '\0')
 		printf("Accept\n");
 	else //else reject
